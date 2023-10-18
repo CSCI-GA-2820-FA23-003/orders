60d0481be480651f11ef7e9daf1702d47c7ac3ef
qm2105 <ashlynx2022@gmail.com>
2023-10-18 18:11:38 +0000

    fix test_find_by_customer_id

--- tests/test_models.py
+++ tests/test_models.py
@@ -156,10 +156,11 @@ class TestOrder(unittest.TestCase):
         self.assertEqual(same_order.name, order.name)
 
     def test_find_by_customer_id(self):
+        """It should find an order list by customer id"""
         order = OrderFactory()
         order.create()
 
-        # Fetch it back by name
+        # Fetch it back by customer_id
         same_order = Order.find_by_customer_id(order.customer_id)[0]
         self.assertEqual(same_order.id, order.id)
         self.assertEqual(same_order.customer_id, order.customer_id)
