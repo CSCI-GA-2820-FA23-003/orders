d4b631f9575efe2221cc483f149ca2ec09472e36
Alex Maoqi Zhang <67251502+MAOQIZHANG@users.noreply.github.com>
2023-12-13 22:40:27 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -374,9 +374,8 @@ class OrderItemResource(Resource):
                 print("found")
                 return item, status.HTTP_200_OK
 
-        # api.abort(status.HTTP_404_NOT_FOUND, "Item not in Order")
-        return "", status.HTTP_404_NOT_FOUND, "Item not in Order"
-
+        api.abort(status.HTTP_404_NOT_FOUND, "Item not in Order")
+        return "", status.HTTP_404_NOT_FOUND
     # PUT /orders/{order_id}/items/{item_id} - updates an Order Item record in the database
     @api.doc("update order item")
     @api.expect(create_item_model, validate=True)
